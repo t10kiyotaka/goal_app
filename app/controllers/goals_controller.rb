77be62172b41e0c9dcb2aae66c0544220d90e311
Kiyotaka <>
2018-11-27 22:59:49 +0900

    Add Goals#new

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,24 @@
 class GoalsController < ApplicationController
   def index
+    @goals = Goal.all
   end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    if @goal.save!
+      redirect_to goals_path
+      flash[:success] = "Successfully Created!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def goal_params
+      params.require(:goal).permit(:description, :priority, :progress_percent)
+    end
 end
