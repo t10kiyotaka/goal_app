9cf97a3caf46006f499331e1b170fa9d9d39b9fc
Kiyotaka <>
2018-11-27 15:38:00 +0900

    Add validation to models

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,8 @@
 class Goal < ApplicationRecord
   belongs_to :user
+  validates :description, presence: true
+  validates :priority, numericality: true
+  validates :progress_percent, numericality: { greater_than_or_equal_to: 0,
+                                               less_than_or_equal_to: 100}
+  
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :goal
+  validates :description, presence: true
+  validates :priority, numericality: true
+  validates :progress_percent, numericality: { greater_than_or_equal_to: 0,
+                                               less_than_or_equal_to: 100}
+  
 end
