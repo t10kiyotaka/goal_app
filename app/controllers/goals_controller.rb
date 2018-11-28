ff78bbfe7f4c3717cb4ddf068b73933ccce75c5d
Kiyotaka <>
2018-11-28 17:57:52 +0900

    Make Goals#destroy

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,16 +1,18 @@
 class GoalsController < ApplicationController
 
-
   def create
     @user = User.find(params[:user_id])
     @user.goals.create!(goal_params)
     redirect_to user_path(@user)
   end
 
-  def edit
+  def destroy
     @user = User.find(params[:user_id])
     @goal = @user.goals.find(params[:id])
+    @goal.destroy
+    redirect_to user_path(@user)
   end
+
 
   private
     def goal_params
