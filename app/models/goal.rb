a17e19f0879c41ce774f2f3c119b6820f65f39ea
Kiyotaka <>
2018-11-29 12:47:26 +0900

    Revise syntax error of validation

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,7 +3,7 @@ class Goal < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
   validates :description, presence: true
-  validates :priority, presence: true, numericality: true, { only_integer: true }
+  validates :priority, presence: true, numericality: { only_integer: true }
   validates :progress_percent, presence: true,
                 numericality: { greater_than_or_equal_to: 0,
                                 less_than_or_equal_to: 100,
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :goal
   validates :description, presence: true
-  validates :priority, presence: true, numericality: true, { only_integer: true }
+  validates :priority, presence: true, numericality: { only_integer: true }
   validates :progress_percent, presence: true,
                 numericality: { greater_than_or_equal_to: 0,
                                 less_than_or_equal_to: 100,
