4d753644d3d78f1c4496631cd4aca9502f7ba8d5
Kiyotaka <>
2018-11-29 23:59:31 +0900

    Make top page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      @user = current_user
-    end
+    # if user_signed_in?
+    #   @user = current_user
+    # end
   end
 end
