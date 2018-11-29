4a8dca03684bfe2473859c7ce92b02af4f468a4a
Kiyotaka <>
2018-11-29 12:48:10 +0900

    Revise syntax error of validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :goal
   validates :description, presence: true
-  validates :priority, numericality: true, { only_integer: true }
+  validates :priority, numericality: { only_integer: true }
   validates :progress_percent, numericality: { greater_than_or_equal_to: 0,
                                                less_than_or_equal_to: 100,
                                                only_integer: true }
