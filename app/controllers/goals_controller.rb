3e8637f853d03e47c1bbc932b93a596f4134ece0
Kiyotaka <>
2018-11-30 13:31:48 +0900

    Make Users#show page

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,34 @@
 class GoalsController < ApplicationController
-  def index
+
+  def create
+    @user = User.find(params[:user_id])
+    @user.goals.create!(goal_params)
+    redirect_to user_path(@user)
   end
+
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+
+  def update
+    @goal = Goal.find(params[:id])
+    if @goal.update(goal_params)
+      redirect_to user_path(current_user)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @goal = Goal.find(params[:id])
+    @goal.destroy
+    redirect_to user_path(current_user)
+  end
+
+
+  private
+    def goal_params
+      params.require(:goal).permit(:description, :priority, :progress_percent)
+    end
+
 end
