053894e4424f1ca73e64dfb3147df06186e38d8b
Kiyotaka <>
2018-12-01 19:40:18 +0900

    Fix atuhentication range

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,5 @@
 class GoalsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   before_action :find_goal, except: [:create]
 
   def create
