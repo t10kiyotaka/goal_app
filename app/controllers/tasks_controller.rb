9ab6fa8f0df467689a9572be0f1d642415e17a06
Kiyotaka <>
2018-12-02 14:32:34 +0900

    Refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,11 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @goal = Goal.find(params[:goal_id])
     @user = User.find_by(id: @goal.user_id)
-    @task = Task.new(description: params[:task][:description],
-                     priority: params[:task][:priority],
-                     progress_percent: params[:task][:progress_percent],
-                     user_id: @user.id,
-                     goal_id: @goal.id)
-    if @task.save!
+    @goal.tasks.build(task_params.merge(user_id: @user.id))
+    if @goal.save!
       redirect_to goal_path(@goal)
     else
       render 'goals/show'
