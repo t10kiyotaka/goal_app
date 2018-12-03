a9dd321a71acb2073c5c440f7468fd3ec785ef1e
Kiyotaka <>
2018-12-04 00:07:19 +0900

    Add Gravatar for user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
   has_many :goals, dependent: :destroy
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  # include MD5 gem, should be part of standard ruby install
+  require 'digest/md5'
+
+  def get_gravatar_url
+    email_address = self.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    image_src = "https://www.gravatar.com/avatar/#{hash}?d=retro"
+    return image_src
+  end
+
 end
