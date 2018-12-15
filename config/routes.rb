05d5dafbc0b5aac038315e7c39f4fe8bdb7b1f1e
Kiyotaka <>
2018-12-16 00:42:48 +0900

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'goals#index'
-  get 'mypage', to: 'loggedin_users#show'
-  get 'mygoals/:id', to: 'loggedin_user_goals#show', as: 'mygoal'
   resources :users, only: [:index, :show], shallow: true do
     resources :goals, except: [:new], shallow: true do
-
       resources :comments, except: [:index, :show, :new]
     end
   end
+
   resource :me, controller: 'me', only: %i[show]
   namespace :my do
     resources :goals, only: %i[show], shallow: true do
