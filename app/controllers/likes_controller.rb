34f0eedf5b27def4adcf59604a82e94ab5a64340
Kiyotaka <>
2018-12-21 13:12:45 +0900

    Replace redirect_to with redirect_back

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,14 +4,14 @@ class LikesController < ApplicationController
   def create
     @goal = Goal.find(params[:goal_id])
     @goal.likes.create!(goal_id: @goal.id, user_id: current_user.id)
-    redirect_to request.referer
+    redirect_back fallback_location: root_url
   end
 
   def destroy
     @like = Like.find_by(goal_id: params[:id], user_id: current_user.id)
     @like.destroy
     @goal = @like.goal
-    redirect_to request.referer
+    redirect_back fallback_location: root_url
   end
 
 end
